52b73d0edffe44ed1fff614ef0888871b308c2df
Aleksei Ivanov <aleksei@majbyr.com>
2024-06-25 14:31:36 +0300

    Added colors; moved UIColor+Extensions.swift;

--- mainapp/UIColor+Extensions.swift
+++ mainapp/UIColor+Extensions.swift
@@ -8,6 +8,9 @@ extension UIColor {
     static let darkActionButton : UIColor = UIColor(red: 0.667, green: 0.667, blue: 0.667, alpha: 0.28)
     static let darkPopup        : UIColor = UIColor(red: 112.0 / 255, green: 112.0 / 255, blue: 112.0 / 255, alpha: 1)
     
+    static let outlineColor     : UIColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+
+    
     @available(iOSApplicationExtension 13.0, *)
     static let dynamicKeyColor = UIColor { traitCollection in
         return traitCollection.userInterfaceStyle == .dark ? .darkButton : .white
@@ -31,5 +34,13 @@ extension UIColor {
     @available(iOSApplicationExtension 13.0, *)
     static let dynamicPopupColor = UIColor { traitCollection in
         return traitCollection.userInterfaceStyle == .dark ? .darkPopup : .white
+    }
+    @available(iOSApplicationExtension 13.0, *)
+    static let dynamicBackgroundColor = UIColor { traitCollection in
+        return traitCollection.userInterfaceStyle == .dark ? .black : .systemGray6
+    }
+    @available(iOSApplicationExtension 13.0, *)
+    static let dynamicFieldColor = UIColor { traitCollection in
+        return traitCollection.userInterfaceStyle == .dark ? .systemGray6 : .white
     }
 }
